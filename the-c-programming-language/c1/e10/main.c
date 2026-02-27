53e85094fbdf4dd3a1586623b579df43496ffe30
Aaron Cassidy <aaron.cassidy1995@gmail.com>
2026-02-27 01:00:27 +0000

    Program to make escape sequences explicit in output

---
+++ the-c-programming-language/c1/e10/main.c
@@ -0,0 +1,33 @@
+// Exercise 1-10: Write a program to copy its input to its output, replacing
+// each tab by \t, each backspace by \b, and each backslash by \\ to make them
+// visible in an unambiguous way.
+
+#include <stdio.h>
+
+int main()
+{
+	int c, esc;
+
+	while ((c = getchar()) != EOF) {
+		esc = 0;
+		if (c == '\t') {
+			putchar('\\');
+			putchar('t');
+			esc = 1;
+		}
+		if (c == '\b') {
+			putchar('\\');
+			putchar('b');
+			esc = 1;
+		}
+		if (c == '\\') {
+			putchar('\\');
+			putchar('\\');
+			esc = 1;
+		}
+		if (esc == 0) {
+			putchar(c);
+		}
+	}
+}
+
