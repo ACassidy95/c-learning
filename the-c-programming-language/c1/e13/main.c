0946eeb797445afaaad130de97377df145e765ab
Aaron Cassidy <aaron.cassidy1995@gmail.com>
2026-02-27 17:45:59 +0000

    Remove unused constants, fix bug where max frequency location is overwritten when the index of the max frequency location is greater than the current word length

--- the-c-programming-language/c1/e13/main.c
+++ the-c-programming-language/c1/e13/main.c
@@ -1,16 +1,13 @@
 // Exercise 1-13a: Write a program to print a histogram of the lengths of words
-// in its input. Horizontal case
+// in its input. Horizontal graph
 
 #include <stdio.h>
-
-#define	STATE_IN	1
-#define STATE_OUT	0
 
 #define RANGE	32
 
 int main()
 {
-	int c, state;
+	int c;
 	int wl, mwlp;
 
 	// wls[0] - Position of most frequent word count
@@ -21,24 +18,20 @@ int main()
 
 	wl	= 0;
 	mwlp	= 0;
-	state	= STATE_OUT;
 	for (int i = 0; i < RANGE; ++i)
 		wls[i] = 0;
 
 	while ((c = getchar()) != EOF) {
-		if (c == ' ' || c == '\n' || c == '\t' ||
-		    c == '.' || c == ',') {
-			state = STATE_OUT;
-			if (wl > 0)
-				++wls[wl];
-
-			// Update position of most frequent length
-			// if necessary
-			if (wls[wl] > wls[wls[0]])
-				wls[0] = wl;
-			wl = 0;
+		if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z')) {
+			++wl;
 		} else {
-			wl++;
+			if (wl > 0) {
+				++wls[wl];
+				if (wls[wl] > wls[wls[0]])
+					wls[0] = wl;
+			}
+
+			wl = 0;
 		}
 	}
 
@@ -64,5 +57,10 @@ int main()
 	printf("%7d", 1);
 	for (int i = 2; i < RANGE; ++i)
 		printf("%3d", i);
-	printf("+\n");
+	printf("+\n\n");
+
+	for(int i = 0; i < RANGE; ++i)
+		if (wls[i] > 0)
+			printf("%3d -- %d\n", i, wls[i]);
+	printf("\n");
 }
