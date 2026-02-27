632afc35941a56ad559e3d4f2bc1eaca9fe990fb
Aaron Cassidy <aaron.cassidy1995@gmail.com>
2026-02-27 17:11:52 +0000

    Print histogram of input frequencies

--- the-c-programming-language/c1/e13/main.c
+++ the-c-programming-language/c1/e13/main.c
@@ -6,41 +6,63 @@
 #define	STATE_IN	1
 #define STATE_OUT	0
 
-#define RANGE	32 // i.e. word lengths 1-31, and 32+
+#define RANGE	32
 
 int main()
 {
 	int c, state;
-	int wl;
+	int wl, mwlp;
 
-	// wls[0] - counts of len 1
-	// wls[RANGE - 1] - counts of len 32+
-	int wls[RANGE];
+	// wls[0] - Position of most frequent word count
+	//		it is useful to not have to search for this
+	// wls[1] - Count of words of length 1
+	// wls[RANGE - 1] - counts of len >= RANGE
+	long wls[RANGE];
 
-	wl = 0;
-	state = STATE_OUT;
-
+	wl	= 0;
+	mwlp	= 0;
+	state	= STATE_OUT;
 	for (int i = 0; i < RANGE; ++i)
 		wls[i] = 0;
 
 	while ((c = getchar()) != EOF) {
-		if (c == ' ' || c == '\n' || c == '\t') {
+		if (c == ' ' || c == '\n' || c == '\t' ||
+		    c == '.' || c == ',') {
 			state = STATE_OUT;
-			if (wl > 0) {
-				// Offset by 1, i.e wls[0]
-				// holds count for len 1, etc.
-				++wls[wl - 1];
-				wl = 0;
-			}
+			if (wl > 0)
+				++wls[wl];
+
+			// Update position of most frequent length
+			// if necessary
+			if (wls[wl] > wls[wls[0]])
+				wls[0] = wl;
+			wl = 0;
 		} else {
 			wl++;
 		}
 	}
 
-	for (int i = 0; i < RANGE; ++i)
-		printf("%3d", wls[i]);
-	printf("\n");
-	for (int i = 0; i < RANGE; ++i)
-		printf("%3d", i + 1);
-	printf("\n");
+	mwlp = wls[0];
+
+	for (int i = 0; i < wls[mwlp]; ++i) {
+		printf("%3d|", wls[mwlp] - i);
+
+		for (int j = 1; j <= RANGE; ++j) {
+			int pc;
+
+			if (wls[j] >= wls[mwlp] - i)
+				pc = '*';
+			else
+				pc = ' ';
+
+			printf("%3c", pc);
+		}
+
+		printf("\n");
+	}
+
+	printf("%7d", 1);
+	for (int i = 2; i < RANGE; ++i)
+		printf("%3d", i);
+	printf("+\n");
 }
