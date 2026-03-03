b307b6a9eedbea03f4bdfc43788efebc5449b1fc
Aaron Cassidy <aaron.cassidy1995@gmail.com>
2026-03-03 01:56:08 +0000

    Update histogram display loop to fit within a certain number of terminal lines

--- the-c-programming-language/c1/e13/main.c
+++ the-c-programming-language/c1/e13/main.c
@@ -3,22 +3,25 @@
 
 #include <stdio.h>
 
-#define RANGE	32
+#define FREQ_RANGE	32
+#define DISP_RANGE	24
 
 int main()
 {
-	int c;
-	int wl, mwlp;
+	int	c;
+	int	wl, maxpos, minpos;
+	float	y, ymin, ymax, ystep;
 
 	// wls[0] - Position of most frequent word count
 	//		it is useful to not have to search for this
 	// wls[1] - Count of words of length 1
-	// wls[RANGE - 1] - counts of len >= RANGE
-	long wls[RANGE];
+	// wls[FREQ_RANGE - 1] - counts of len >= FREQ_RANGE
+	long wls[FREQ_RANGE];
 
 	wl	= 0;
-	mwlp	= 0;
-	for (int i = 0; i < RANGE; ++i)
+	maxpos	= 0;
+	minpos	= 1;
+	for (int i = 0; i < FREQ_RANGE; ++i)
 		wls[i] = 0;
 
 	while ((c = getchar()) != EOF) {
@@ -27,40 +30,58 @@ int main()
 		} else {
 			if (wl > 0) {
 				++wls[wl];
-				if (wls[wl] > wls[wls[0]])
+				if (wls[wl] > wls[maxpos]) {
 					wls[0] = wl;
+					maxpos = wl;
+				}
 			}
 
 			wl = 0;
 		}
 	}
 
-	mwlp = wls[0];
+	maxpos = wls[0];
+	for (int i = 1; i < FREQ_RANGE; ++i)
+		if (wls[i] < wls[minpos] && wls[i] > 0)
+			minpos = i;
 
-	for (int i = 0; i < wls[mwlp]; ++i) {
-		printf("%3d|", wls[mwlp] - i);
+	ymin	= wls[minpos];
+	ymax	= wls[maxpos];
 
-		for (int j = 1; j <= RANGE; ++j) {
+	int dr = DISP_RANGE;
+	if (ymax < DISP_RANGE)
+		dr = ymax;
+	ystep	= (ymax - ymin) / dr;
+
+	for (int i = 0; i <= dr; ++i) {
+		y = ymax - (i * ystep);
+		printf("%6.0f|", y);
+
+		for (int j = 1; j < FREQ_RANGE; ++j) {
 			int pc;
+			if (wls[j] >= wls[minpos]) {
+				if (wls[j] == wls[maxpos] - (i * ystep))
+					pc = '.';
+				else if (wls[j] >= wls[maxpos] - (i * ystep))
+					pc = '*';
+				else
+					pc = ' ';
 
-			if (wls[j] >= wls[mwlp] - i)
-				pc = '*';
-			else
-				pc = ' ';
-
-			printf("%3c", pc);
+				printf("%3c", pc);
+			} else {
+				++j;
+			}
 		}
 
 		printf("\n");
 	}
 
-	printf("%7d", 1);
-	for (int i = 2; i < RANGE; ++i)
-		printf("%3d", i);
-	printf("+\n\n");
+	printf("%10d", 1);
+	for (int i = 2; i < FREQ_RANGE; ++i)
+		if (wls[i] >= wls[minpos])
+			printf("%3d", i);
 
-	for(int i = 0; i < RANGE; ++i)
-		if (wls[i] > 0)
-			printf("%3d -- %d\n", i, wls[i]);
-	printf("\n");
+	printf("\n\nMax: %5d Min: %5d Mode: %5d Display Range: %5d\n"
+	       ,wls[maxpos], wls[minpos], maxpos, dr);
+	printf("ymax: %5f, ymin: %5f, ystep: %5f\n", ymax, ymin, ystep);
 }
