890a14c778b86cdff200bb03ba52f8d28d40b344
Aaron Cassidy <aaron.cassidy1995@gmail.com>
2026-03-06 22:24:15 +0000

    Comment removal program

--- the-c-programming-language/c1/e23/main.c
+++ the-c-programming-language/c1/e23/main.c
@@ -2,9 +2,56 @@
 // Don't forget to handle quoted strings and character constants properly. C
 // comments don't nest.
 
+/* Trad comment */
+
+/* Multiline
+ * comment
+ */
+
 #include <stdio.h>
+
+#define MAXLINE	1024
 
 int main()
 {
-	return 0;
+	int incmt, lpos;
+	char c, lka; // An in-line comment
+
+	incmt = 0;
+	lpos = 0;
+	while((c = getchar()) != EOF) {
+		if (c == '/') {
+			lka = getchar();
+
+			if (lka == '*') {
+				if (incmt == 0)
+					incmt = 1;
+
+				while (incmt == 1) {
+					while ((c = getchar()) != '*')
+						;
+					lka = getchar();
+
+					if (lka == '/')
+						incmt = 0;
+				}
+			} else if (lka == '/') {
+				while ((c = getchar()) != '\n')
+					;
+				if (lpos > 0) {
+					putchar(c);
+					++lpos;
+				}
+			} else {
+				putchar(c);
+				putchar(lka);
+				lpos = lpos + 2;
+			}
+		} else {
+			if (!(c == '\n' && lpos == 0))
+				putchar(c);
+			lpos++;
+		}
+	}
 }
+
