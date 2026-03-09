69fe9fdaab67a7bfb0ee53517fc1e34a1c9a8f83
Aaron Cassidy <aaron.cassidy1995@gmail.com>
2026-03-09 22:17:12 +0000

    Add bitcount program with original one's complement impl and added two's complement impl

---
+++ the-c-programming-language/c2/e9/main.c
@@ -0,0 +1,77 @@
+// Exercise 2-9: In a two's complement number system x &= (x-1) deletes the
+// rightmost 1-bit in x. Explain why, use this observation to write a faster
+// version of bitcount
+
+#include <stdio.h>
+
+#define MAXBUF 32
+
+int get_num(char s[], int max);
+int atoi(char s[]);
+int bitcount_oc(unsigned x);
+int bitcount_tc(unsigned x);
+
+int main()
+{
+	unsigned int	lx, x;
+	int		bco, bcf;
+	char		sx[MAXBUF];
+
+	printf("Enter X value: ");
+	while ((lx = get_num(sx, MAXBUF)) > 0) {
+		x = atoi(sx);
+		bco = bitcount_oc(x);
+		bcf = bitcount_tc(x);
+
+		printf("Original Bitcount value: %d\n", bco);
+		printf("Fast Bitcount value: %d\n", bcf);
+
+		printf("Enter X value: ");
+	}
+
+	return 0;
+}
+
+int get_num(char s[], int max)
+{
+	int i, c;
+
+	for (i = 0; i < max - 1 && (c = getchar()) != EOF && c != '\n'; ++i)
+		s[i] = c;
+
+	// No need to preserve newline in a number
+	s[i] = '\0';
+	return i;
+}
+
+int atoi(char s[])
+{
+	int i, n;
+
+	n = 0;
+	for (i = 0; s[i] != '\0'; ++i)
+		n = n * 10 + (s[i] - '0');
+
+	return n;
+}
+
+int bitcount_oc(unsigned x)
+{
+	int b;
+
+	for (b = 0; x != 0; x >>= 1)
+		if (x & 0x1)
+			++b;
+
+	return b;
+}
+
+int bitcount_tc(unsigned x)
+{
+	int b;
+
+	for (b = 0; x != 0; x &= (x - 1))
+		++b;
+
+	return b;
+}
