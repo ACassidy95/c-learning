d24cdc5904584aa782a7d525add22b0f0738aa44
Aaron Cassidy <aaron.cassidy1995@gmail.com>
2026-03-09 01:27:39 +0000

    Bit inversion initial commit

---
+++ the-c-programming-language/c2/e7/main.c
@@ -0,0 +1,16 @@
+// Exercise 2-7: Write a function invert(x, p, n) that returns x with the n bits
+// that begin at position p inverted leaving the others unchanged.
+
+#include <stdio.h>
+
+#define MAXBUF		32
+#define PARAMBUF	4
+
+int get_num(char s[], int max);
+int atoi(char s[]);
+int invert(int x, int p, int n);
+
+int main()
+{
+	return 0;
+}
