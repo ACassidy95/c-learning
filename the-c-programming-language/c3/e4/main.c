145cc6e4e540469a5808903962c80f623ff6a50a
Aaron Cassidy <aaron.cassidy1995@gmail.com>
2026-03-11 23:09:47 +0000

    E3-4: Add input functions

--- the-c-programming-language/c3/e4/main.c
+++ the-c-programming-language/c3/e4/main.c
@@ -7,13 +7,59 @@
 
 #define MAXBUF 64
 
-int get_num(char s[], int maxbuf);
-void reverse(char s[]);
-int atoi(char s[]);
-void itoa_orig(int n, char s[]);
-void itoa_new(int n, char s[]);
+int	get_num(char s[], int maxbuf);
+int	atoi(char s[]);
+void	itoa_orig(int n, char s[]);
+void	itoa_new(int n, char s[]);
+void	reverse(char s[]);
 
 int main()
 {
+	int	len, xo, xn;
+	char	s[MAXBUF], so[MAXBUF], sn[MAXBUF];
+
+	while ((len = get_num(s, MAXBUF)) > 0) {
+		xo = atoi(s);
+		xn = xo;
+
+		itoa_orig(xo, so);
+		printf("X value (itoa original): %d\n", xo);
+
+		itoa_new(xn, sn);
+		printf("X value (itoa new): %d\n", xn);
+	}
+
 	return 0;
+}
+
+int get_num(char s[], int maxbuf)
+{
+	int c, i;
+
+	for (i = 0; i < maxbuf - 1 && (c = getchar()) != EOF && c != '\n'; ++i)
+		s[i] = c;
+
+	s[i] = '\0';
+	return i;
+}
+
+int atoi(char s[])
+{
+	int n, i;
+
+	n = 0;
+	for (i = 0; s[i] != '\0'; ++i)
+		n = (n * 10) + (s[i] - '0');
+
+	return n;
+}
+
+void itoa_orig(int n, char s[])
+{
+	return;
+}
+
+void itoa_new(int n, char s[])
+{
+	return;
 }
