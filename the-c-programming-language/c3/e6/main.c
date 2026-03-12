0b98a875a43a196607d3259b0aee49fd06cecfbe
Aaron Cassidy <aaron.cassidy1995@gmail.com>
2026-03-12 19:20:25 +0000

    E3-6: Add itoa implementation which has a minimum width filler functionality

--- the-c-programming-language/c3/e6/main.c
+++ the-c-programming-language/c3/e6/main.c
@@ -2,16 +2,28 @@
 // two. The third argument is a miniumum field width; the converted nmuber must
 // be padded with blanks on the left if necessary to make it wide enough.
 
+#include <limits.h>
 #include <stdio.h>
+#include <string.h>
 
-#define MAX_BUF 32
+#define MAX_BUF 8
 
 int	get_num(char s[], int max);
 int	atoi(char s[]);
-void	reverse(char s[], int len);
+void	reverse(char s[]);
+void	itoaw(int x, char s[], unsigned w);
 
 int main()
 {
+	int	len, x;
+	char	s[MAX_BUF];
+
+	while ((len = get_num(s, MAX_BUF)) > 0) {
+		x = atoi(s);
+		itoaw(x, s, MAX_BUF);
+		printf("%s\n", s);
+	}
+
 	return 0;
 }
 
@@ -36,7 +48,7 @@ int atoi(char s[])
 		sidx = 1;
 
 	n = 0;
-	for (i = bgn; s[i] != '\0'; ++i)
+	for (i = sidx; s[i] != '\0'; ++i)
 		n = (n * 10) + (s[i] - '0');
 
 	if (sidx)
@@ -45,19 +57,53 @@ int atoi(char s[])
 	return n;
 }
 
-void reverse(char s[], int len)
+void reverse(char s[])
 {
-	int i, l;
+	int i, l, len, off, bnd;
 
 	i = 0;
-	if (s[0] == '-')
+	off = 1;
+	if (s[0] == '-') {
 		i = 1;
+		off = 0;
+	}
 
-	for (; i = len / 2; ++i) {
-		l = len - i - 1;
+	len = strlen(s);
+	bnd = (len + i) / 2;
+	for (i; i < bnd; ++i) {
+		l = len - i - off;
 
 		s[i] ^= s[l];
 		s[l] ^= s[i];
 		s[i] ^= s[l];
 	}
+}
+
+void itoaw(int x, char s[], unsigned w)
+{
+	int i, sign;
+
+	if ((sign = x) < 0) {
+		if (x == INT_MIN)
+			x = -x - 1;
+		else
+			x = -x;
+	}
+
+	i = 0;
+	do {
+		s[i++] = x % 10 + '0';
+	} while ((x /= 10) > 0);
+
+	if (sign < 0) {
+		s[i++] = '-';
+	}
+
+	while (i < w)
+		s[i++] = '.';
+
+	s[i] = '\0';
+	reverse(s);
+
+	return;
 }
