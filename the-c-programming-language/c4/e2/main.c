0dd0b34b6fac8e01ae4421b9c4b562ffe8617247
Aaron Cassidy <aaron.cassidy1995@gmail.com>
2026-04-06 18:44:40 +0100

    4.2: Update atof to extend to scientific notation

--- the-c-programming-language/c4/e2/main.c
+++ the-c-programming-language/c4/e2/main.c
@@ -1,9 +1,12 @@
+#include <ctype.h>
 #include <stdio.h>
 
 #define MAXLINE 1024
 
 int	get_line(char s[], int lim);
 double	atof(char s[]);
+double	pow(double base, double exp);
+double	dabs(double x);
 
 int main()
 {
@@ -33,4 +36,77 @@ int get_line(char s[], int lim)
 	s[i] = '\0';
 
 	return i;
+}
+
+double atof(char s[])
+{
+	double	f, val, power, exp, exppow;
+	int	i, sign, expsign, scinot;
+
+	for(i = 0; isspace(s[i]); ++i)
+		;
+
+	scinot = -1;
+	sign = (s[i] == '-') ? -1 : 1;
+	if (s[i] == '+' || s[i] == '-')
+		++i;
+
+	for (val = 0.0; isdigit(s[i]); ++i)
+		val = 10.0 * val + (s[i] - '0');
+
+	if (s[i] == '.')
+		++i;
+	for (power = 1.0; isdigit(s[i]); ++i) {
+		val = 10.0 * val + (s[i] - '0');
+		power *= 10.0;
+	}
+
+	f = sign * val / power;
+
+	if (s[i] == 'e' || s[i] == 'E') {
+		++i;
+		scinot = 1;
+	}
+
+	expsign = (s[i] == '-') ? -1 : 1;
+	if(s[i] == '+' || s[i] == '-')
+		++i;
+
+	for (exp = 0.0; isdigit(s[i]); ++i)
+		exp = 10.0 * exp + (s[i] - '0');
+
+	if (scinot) {
+		f *= pow(10.0, expsign * exp);
+	}
+
+	return f;
+}
+
+double pow(double base, double exp)
+{
+	double	p, a;
+	int	i;
+
+	p = 1.0;
+	a = dabs(exp);
+	for (i = 0; i < a; ++i) {
+		if (exp < 0)
+			p /= base;
+		else
+			p *= base;
+	}
+
+	return p;
+}
+
+double dabs(double x)
+{
+	double xx;
+
+	if (x >= 0)
+		xx = x;
+	else
+		xx = x * -1.0;
+
+	return xx;
 }
