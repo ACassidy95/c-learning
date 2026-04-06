14b0f81e8c6bbc97bfe26bf5e5c8cafee32fee0c
Aaron Cassidy <aaron.cassidy1995@gmail.com>
2026-04-06 18:08:57 +0100

    4.2: Add getline

--- the-c-programming-language/c4/e2/main.c
+++ the-c-programming-language/c4/e2/main.c
@@ -1,6 +1,36 @@
 #include <stdio.h>
 
+#define MAXLINE 1024
+
+int	get_line(char s[], int lim);
+double	atof(char s[]);
+
 int main()
 {
+	double	d, sum;
+	char	line[MAXLINE];
+
+	sum = 0;
+	while (get_line(line, MAXLINE) > 0) {
+		d = atof(line);
+		printf("%lf: %lf + %lf = %lf\n", d, sum, d, sum + d);
+		sum += d;
+	}
+
 	return 0;
+}
+
+int get_line(char s[], int lim)
+{
+	int	i;
+	char	c;
+
+	i = 0;
+	while (--lim > 0 && (c = getchar()) != EOF && c != '\n')
+		s[i++] = c;
+	if (c == '\n')
+		s[i++] = c;
+	s[i] = '\0';
+
+	return i;
 }
