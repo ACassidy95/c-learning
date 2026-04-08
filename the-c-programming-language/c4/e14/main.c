6ca329c38e05d41022e10e44a9994bf0f8bcb423
Aaron Cassidy <aaron.cassidy1995@gmail.com>
2026-04-09 00:39:03 +0100

    4-14: Add macro to swap variables of multiple types

--- the-c-programming-language/c4/e14/main.c
+++ the-c-programming-language/c4/e14/main.c
@@ -3,7 +3,30 @@
 
 #include <stdio.h>
 
+#define swap(t, x, y) \
+{ \
+	t tmp; \
+	tmp = x; \
+	x = y; \
+	y = tmp; \
+}
+
 int main(void)
 {
+	int	m, n;
+	double	x, y;
+
+	m = 0;
+	n = 1;
+	x = 0.0;
+	y = 1.0;
+
+	printf("m = %d, n = %d, x = %.2lf, y = %.2lf\n", m, n, x, y);
+
+	swap(int, m, n);
+	swap(double, x, y);
+
+	printf("m = %d, n = %d, x = %.2lf, y = %.2lf\n", m, n, x, y);
+
 	return 0;
 }
