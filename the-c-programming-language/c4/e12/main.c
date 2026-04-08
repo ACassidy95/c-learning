1b5c0683df49c85dc26c5cabea64c9e73a47d356
Aaron Cassidy <aaron.cassidy1995@gmail.com>
2026-04-09 00:45:25 +0100

    4.12: Initial commit.

---
+++ the-c-programming-language/c4/e12/main.c
@@ -0,0 +1,17 @@
+// Exercise 4.12: Write a recursive version of itoa, that is, convert an integer
+// to a string by recursive calls to the routine itoa.
+
+#include <stdio.h>
+
+void itoa(int n, char s[]);
+
+int main(void)
+{
+	return 0;
+}
+
+void itoa(int n, char s[])
+{
+	return;
+}
+
