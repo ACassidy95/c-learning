a7679bcaf773acf93bd56a9083cf06d69b612cea
Aaron Cassidy <aaron.cassidy1995@gmail.com>
2026-04-09 01:55:07 +0100

    4-13: Add function reverse to recursively reverse strings

--- the-c-programming-language/c4/e13/main.c
+++ the-c-programming-language/c4/e13/main.c
@@ -2,9 +2,54 @@
 // reverses the string s in place.
 
 #include <stdio.h>
+#include <string.h>
+
+#define MAXLINE 1024
+
+int get_line(char [], int);
+void reverse(char [], int);
 
 int main(void)
 {
+	int	len;
+	char	line[MAXLINE];
+
+	while((len = get_line(line, MAXLINE))) {
+		reverse(line, len);
+		printf("%s\n", line);
+	}
+
 	return 0;
 }
 
+int get_line(char s[], int lim)
+{
+	int c, i;
+
+	i = 0;
+	while (--lim > 0 && (c = getchar()) != EOF && c != '\n')
+		s[i++] = c;
+	if (c == '\n')
+		s[i++] = '\n';
+	s[i] = '\0';
+
+	return i;
+}
+
+void reverse(char s[], int len)
+{
+	int st, e, t;
+
+	st = 0;
+	e = len - 1;
+
+	if (st < e) {
+		t = s[st];
+		s[st] = s[e];
+		s[e] = t;
+
+		reverse(s + 1, e - 1);
+	}
+
+	return;
+}
