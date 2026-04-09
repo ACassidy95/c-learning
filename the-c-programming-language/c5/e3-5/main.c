68bc38cd8bb3e44e514af6efb7ad549f2a754ee3
Aaron Cassidy <aaron.cassidy1995@gmail.com>
2026-04-10 00:40:56 +0100

    5-(3-5): Add function str_end to determine if one string is suffix of another

--- the-c-programming-language/c5/e3-5/main.c
+++ the-c-programming-language/c5/e3-5/main.c
@@ -4,6 +4,7 @@
 
 int get_line(char *, int);
 int str_cat(char *, char *);
+int str_end(char *, char *);
 int str_len(char*);
 
 int main(void)
@@ -13,6 +14,10 @@ int main(void)
 
 	while (sl = get_line(s, MAXSTR)) {
 		tl = get_line(t, MAXSTR);
+
+		if (str_end(s, t))
+			printf("\"%s\" occurs at the end of \"%s\" without concatenation\n", t, s);
+
 		cl = str_cat(s, t);
 		printf("Concat: %s\nlen(s): %d\nlen(t): %d\nlen(s#t): %d\n",
 		       s, sl, tl, cl);
@@ -37,6 +42,8 @@ int get_line(char *s, int lim)
 	return n;
 }
 
+// Concatenates the contents of s and t and stores the result in s,
+// while returning the length of the combined string.
 int str_cat(char *s, char *t)
 {
 	int i, sl;
@@ -51,6 +58,27 @@ int str_cat(char *s, char *t)
 	*(s + sl + i) = '\0';
 
 	return sl + i;
+}
+
+// Determines if t occurs at the end of s. Returns 1 if so and 0 otherwise.
+int str_end(char *s, char *t)
+{
+	int i, j, sl, tl;
+
+	sl = str_len(s);
+	tl = str_len(t);
+
+	if (tl > sl) {
+		printf("Error - str_end: Search string longer than target string %d > %d.\n", tl, sl);
+		return 0;
+	}
+
+	s += sl - tl;
+	for ( ; *s && *t && *s == *t; s++, t++)
+		;
+	if (*t)
+		return 0;
+	return 1;
 }
 
 int str_len(char *s)
