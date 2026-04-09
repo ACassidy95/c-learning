a46a14bace3e547cf6038a91f6579bf31c47b490
Aaron Cassidy <aaron.cassidy1995@gmail.com>
2026-04-09 23:25:07 +0100

    5-(3-5): Add input retrieval

--- the-c-programming-language/c5/e3-5/main.c
+++ the-c-programming-language/c5/e3-5/main.c
@@ -2,11 +2,27 @@
 
 #define MAXSTR	1024
 
+int getline(char *, int);
 int strcat(char *, char *);
 
 int main(void)
 {
 	return 0;
+}
+
+int getline(char *s, int lim)
+{
+	int c, n;
+
+	n = 0;
+
+	while (--lim > 0 && (c = getchar()) != EOF && c != '\n')
+		(s + n++) = c;
+	if (c == '\n')
+		(s + n++) = c;
+	(s + n) = '\0';
+
+	return n;
 }
 
 int strcat(char *s, char *t)
