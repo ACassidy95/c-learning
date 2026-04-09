7ea369b4e75712a3029ab304476064534a2aad5c
Aaron Cassidy <aaron.cassidy1995@gmail.com>
2026-04-09 21:59:12 +0100

    5-1: Initial commit

---
+++ the-c-programming-language/c5/e1/main.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int	getch(void);
+int	getint(int *);
+void	ungetch(int);
+
+int main(void)
+{
+	return 0;
+}
+
+int pushback = '\0';
+
+int getch(void)
+{
+	return 0;
+}
+
+void ungetch(int c)
+{
+	return;
+}
