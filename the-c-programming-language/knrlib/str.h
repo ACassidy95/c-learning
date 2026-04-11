bc8fa6ca8c2470341ba8ca8acbc2dfcd540e85c8
Aaron Cassidy <aaron.cassidy1995@gmail.com>
2026-04-12 00:51:15 +0100

    Fix missing semicolon and mismatched function signatures

--- the-c-programming-language/knrlib/str.h
+++ the-c-programming-language/knrlib/str.h
@@ -5,8 +5,8 @@
 
 size_t	str_cat(char *, char*);
 size_t	str_len(char *);
-int	str_ncmp(char *, char *);
-size_t	str_ncpy(char *, char *)
+int	str_ncmp(char *, char *, size_t n);
+size_t	str_ncpy(char *, char *, size_t n);
 void	str_rev(char *);
 
 #endif
