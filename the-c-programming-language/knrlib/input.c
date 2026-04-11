e53f28e93504380ae126d867224dd0903216f49b
Aaron Cassidy <aaron.cassidy1995@gmail.com>
2026-04-11 23:35:56 +0100

    Implement getln

--- the-c-programming-language/knrlib/input.c
+++ the-c-programming-language/knrlib/input.c
@@ -5,6 +5,8 @@
 /* Pushback character for getch/ungetch*/
 static int pb = -1;
 
+/* Read character pushed back by ungetch() if one exists,
+ * otherwise read a character from stdin. */
 int getch(void)
 {
 	int c;
@@ -19,6 +21,7 @@ int getch(void)
 	return c;
 }
 
+/* Return character to be re-read by getch. */
 void ungetch(int c)
 {
 	if (pb != -1)
@@ -26,5 +29,22 @@ void ungetch(int c)
 	else
 		pb = c;
 	return;
+}
+
+/* Read from stdin until \n encountered or max size reached and return line
+ * length (excluding null terminator). */
+size_t getln(char *s, size_t max)
+{
+	size_t l;
+
+	for (l = 0; max > 0 && (c = getch()) != EOF && c != '\n'; ++l, --max)
+		*s++ = c;
+	if (c == '\n') {
+		*s++ = c;
+		++l;
+	}
+	*s = '\0';
+
+	return l;
 }
 
