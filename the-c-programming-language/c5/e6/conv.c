f2bb4d780f97625f285a5883f93a80f17d060887
Aaron Cassidy <aaron.cassidy1995@gmail.com>
2026-04-11 15:13:22 +0100

    5-6: Declarations for conversion functions

---
+++ the-c-programming-language/c5/e6/conv.c
@@ -0,0 +1,22 @@
+#include <stddef.h>
+#include "conv.h"
+
+int atoi(char *s)
+{
+	return 0;
+}
+
+double atof(char *s)
+{
+	return 0;
+}
+
+char* itoa(int n)
+{
+	return NULL;
+}
+
+char* ftoa(double f)
+{
+	return NULL;
+}
---
+++ the-c-programming-language/c5/e6/conv.h
@@ -0,0 +1,13 @@
+#ifndef __CONV_H
+#define __CONV_H
+
+/* Converts a given string to integer/floating-point values.
+ * Return 0 if bad input received. */
+int	atoi(char *);
+double	atof(char *);
+
+/* Converts a given integer/floating-point value to string. */
+char*	itoa(int);
+char*	ftoa(double);
+
+#endif
