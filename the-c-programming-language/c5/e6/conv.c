9280657f09b98ec17677641b5984ee69d45a61e5
Aaron Cassidy <aaron.cassidy1995@gmail.com>
2026-04-11 15:26:01 +0100

    5-6: Add pointer-based atoi implementation

--- the-c-programming-language/c5/e6/conv.c
+++ the-c-programming-language/c5/e6/conv.c
@@ -1,9 +1,32 @@
+#include <ctype.h>
 #include <stddef.h>
 #include "conv.h"
 
 int atoi(char *s)
 {
-	return 0;
+	char	*st;
+	int	n, sign;
+
+	// Swallow whitespace
+	for (st = s; isspace(*st); ++st)
+		;
+
+	// Not a number
+	if (!isdigit(*st) && *st != '+' && st != '-') {
+		printf("Error - atoi: Non-numerical character %c detected in input",
+		       *st);
+		return 0;
+	}
+
+	sign = (*st == '-') ? -1 : 1;
+	if (*st == + || *st == -1)
+		++st;
+
+	for (n = 0; isdigit(*st); ++st)
+		n = 10 * n + (*st - '0');
+	n *= sign;
+
+	return n;
 }
 
 double atof(char *s)
